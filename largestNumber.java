70eb2645a3c49884d2ccae3bb1abf1e8bd76df81
trinidad-christian <156033133+trinidad-christian@users.noreply.github.com>
2025-04-05 03:36:36 +0000

    FInished the problem for largestnumber.java

--- largestNumber.java
+++ largestNumber.java
@@ -9,13 +9,16 @@ The maximum number is: 9
 public class largestNumber {
     public static void main(String[] args) {
         int[] numbers = {3, 7, 2, 9, 5};
+
 //Before line 12 it's everything we are given in the problem
 
         int largest = 0;
 
         for (int i = 0; i < numbers.length ; i++) {
-            
+            if (numbers[i] > largest)
+                largest = numbers[i];
         }
+        System.out.print(largest);
     }
 }
 
