ccacc3d32cff6aa6928c0d90e5f5bfd37a6630af
trinidad-christian <156033133+trinidad-christian@users.noreply.github.com>
2025-04-05 03:27:06 +0000

    comments on code

--- largestNumber.java
+++ largestNumber.java
@@ -9,6 +9,7 @@ The maximum number is: 9
 public class largestNumber {
     public static void main(String[] args) {
         int[] numbers = {3, 7, 2, 9, 5};
+//Before line 12 it's everything we are given in the problem
 
         int largest = 0;
 
