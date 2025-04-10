ed64ac50d0ce5d768c4f4bb9fca95e812cb6560e
trinidad-christian <156033133+trinidad-christian@users.noreply.github.com>
2025-04-10 14:02:19 +0000

    Adding greating user

--- calculator.java
+++ calculator.java
@@ -3,7 +3,9 @@ import java.util.Scanner;
 public class calculator {
     public static void main (String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.print("");
+        System.out.print("Welcome to Trinidad's calculator what's your name?");
+        String name = scan.nextLine(); // Scans the name (String) and stores it (add to notes)
+
 
 
 
