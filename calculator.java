bf39484b952c606890a6aa3118fd83cd10de15ac
trinidad-christian <156033133+trinidad-christian@users.noreply.github.com>
2025-04-10 14:09:22 +0000

    starting on calculator

--- calculator.java
+++ calculator.java
@@ -3,10 +3,12 @@ import java.util.Scanner;
 public class calculator {
     public static void main (String[] args) {
         Scanner scan = new Scanner(System.in);
+
         System.out.print("Welcome to Trinidad's calculator what's your name?");
         String name = scan.nextLine(); // Scans the name (String) and stores it (add to notes)
-
-
+        //Gets the operations the user whats to do and the numbers
+        System.out.println("Hello, " + name + " what operations are you going to do today?")
+        
 
 
 
