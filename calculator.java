db7e259cfb6b7801bdffc2ca90c150668e378cb2
trinidad-christian <156033133+trinidad-christian@users.noreply.github.com>
2025-04-10 13:23:13 +0000

    Initial changes

--- calculator.java
+++ calculator.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class calculator {
-    
+    public static void main (String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("");
+
+
+
+
+        scan.close();
+    }
 }
