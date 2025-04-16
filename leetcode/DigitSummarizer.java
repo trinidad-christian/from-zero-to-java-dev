2075b948db0a59c7aa41a16994ea793774bd3318
TrinidadLabs <156033133+trinidad-christian@users.noreply.github.com>
2025-04-16 17:17:14 +0000

    added my solution for DigitSummarizer.java

--- leetcode/DigitSummarizer.java
+++ leetcode/DigitSummarizer.java
@@ -1,1 +1,36 @@
+public class DigitSummarizer
+{
+
+  /** Returns the single-digit "digital root" of n
+   *  Precondition: n is a positive number
+   */
+  public static int digitalRoot(int n)
+  {
+    //If the int value has 2 or more digits then it runs until we have one
+    int root = n;
+    while (root>=10) {
+      root = digitSum(root); //after this we would have 15
+    }
+    return root;
+    
+  }
+
+  /** Returns the sum of the digits in n
+   *  Precondition: n is a positive number
+   */
+  public static int digitSum(int n)
+  {
+    /* COMPLETE WORKING METHOD PROVIDED */
+    /* DO NOT MODIFY THE IMPLEMENTATION OF THIS METHOD*/
+    int s = 0;
+    while(n > 0){
+      s += n%10;
+      n /=10;
+    }
+    return s;
+  }
+
+  /* Methods for subsequent parts of this question are not shown */
+
+}
 
