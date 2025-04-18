d07f5959b70476280a923b7b809b1a16d6525de3
TrinidadLabs <156033133+trinidad-christian@users.noreply.github.com>
2025-04-18 18:01:46 -0500

    Update Tree.java

--- projects/Tree.java
+++ projects/Tree.java
@@ -1,1 +1,22 @@
+public class Tree {
+  
+   public static void main(String[] args) {
 
+    //In this method I will introduce myself and print a tree to the screen.
+    System.out.println("Hello there, I'm Christian Trinidad and I'm learning to code in Java!");
+    System.out.println("Today, I will be planting a tree!!");
+
+    //Planting that tree
+   System.out.println("      *");
+   System.out.println("      *");
+   System.out.println("     ***");
+   System.out.println("     ***");
+   System.out.println("    *****");
+   System.out.println("    *****");
+   System.out.println("      *");
+   System.out.println("#################");
+   }
+   
+   
+
+}
