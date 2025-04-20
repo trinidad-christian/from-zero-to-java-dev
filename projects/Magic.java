ddeff04e7304978209cb1de3187742f085d3faf5
TrinidadLabs <156033133+trinidad-christian@users.noreply.github.com>
2025-04-20 01:29:34 +0000

    finished magic project

--- projects/Magic.java
+++ projects/Magic.java
@@ -1,1 +1,18 @@
+public class Magic {
+    public static void main (String[] args) {
+        int myNumber = 21;
+        /*
+        We will refer to myNumber as the original number
+        from now on
+        */
 
+        int stepOne = myNumber * myNumber;
+        int stepTwo = stepOne + myNumber;
+        int stepThree = stepTwo/myNumber;
+        int stepFour = stepThree + 17;
+        int stepFive = stepFour - myNumber;
+        int stepSix = stepFive/6;
+
+        System.out.println(stepSix);
+    }
+}
